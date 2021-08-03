78257543385934fe431bdf6f1cfb9fc6803b1e6c
Cansiny0320 <1136843910@qq.com>
2021-08-03 22:50:45 +0800

    feat: 列表分页

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,22 +1,31 @@
 import { View, Text, Image } from "@tarojs/components";
-import { getStorage, setStorage, useDidShow } from "@tarojs/taro";
+import {
+  getStorage,
+  setStorage,
+  useDidShow,
+  useReachBottom,
+} from "@tarojs/taro";
 import { useState } from "react";
 import { List } from "../common/types";
 import Item from "./components/Item";
 import styles from "./index.modules.scss";
-import { STATUS, Storage } from "../common/constants";
+import { Storage } from "../common/constants";
 import { getInitList } from "../common/helpers";
+import ApplyImg from "../../static/input.png";
 
 const initList: List = getInitList();
-
+const ONE_PAGE_NUM = 10;
 const Index = () => {
+  const [page, setPage] = useState(1);
+  const [totalList, setTotalList] = useState<List>(initList);
   const [list, setList] = useState<List>(initList);
   const [name, setName] = useState("姓名");
   useDidShow(() => {
     getStorage({
       key: Storage.LIST,
       success: (res) => {
-        setList(res.data);
+        setTotalList(res.data);
+        setList(res.data.slice(0, ONE_PAGE_NUM * page));
       },
       fail: () => {
         setStorage({
@@ -33,21 +42,32 @@ const Index = () => {
     });
   });
 
-  const handleApply = () => {
-    if (list[0].status === STATUS.BACK) {
-      const newList = [...initList, ...list];
-      setList(newList);
-      setStorage({
-        key: Storage.LIST,
-        data: newList,
-      });
+  useReachBottom(() => {
+    if (totalList.length > page * ONE_PAGE_NUM) {
+      setList(totalList.slice(0, ONE_PAGE_NUM * (page + 1)));
+      setPage(page + 1);
     }
+  });
+
+  const handleApply = () => {
+    const newList = [
+      {
+        ...initList[0],
+        id: list[0].id + 1,
+      },
+      ...list,
+    ];
+    setList(newList);
+    setStorage({
+      key: Storage.LIST,
+      data: newList,
+    });
   };
 
   return (
     <View className={styles.apply}>
-      <Image src={require("../../static/input.png")} onClick={handleApply} />
-      <Text className={styles.count}>申请记录 ({list.length}条)</Text>
+      <Image src={ApplyImg} onClick={handleApply} />
+      <Text className={styles.count}>申请记录 ({totalList.length}条)</Text>
       {list.map((e) => (
         <Item
           name={name}
