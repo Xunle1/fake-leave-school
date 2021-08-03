2a94cadd536a85a50eaa24248ae582343bab6929
Cansiny0320 <1136843910@qq.com>
2021-08-03 22:28:19 +0800

    chore: add res types

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,20 @@
-import { request } from "@tarojs/taro";
+import { request, RequestTask } from "@tarojs/taro";
 import { Type, Location } from "src/pages/common/constants";
 
 const json2base64 = (json: any) =>
   Buffer.from(JSON.stringify(json)).toString("base64");
 const getNowTimestamp = () => Math.round(+new Date() / 1000);
+
+interface ApplyRes {
+  status: number;
+  message: string;
+  data: {
+    time: string;
+    type: Type;
+    location: Location;
+    num: number;
+  };
+}
 
 const baseData = {
   version: "1.1",
@@ -20,8 +31,8 @@ const requestData = {
   log_id: 1,
 };
 
-export const apply = (type: Type, location: Location) => {
-  return request({
+export const apply = (type: Type, location: Location): RequestTask<ApplyRes> =>
+  request({
     url: "https://we.cqupt.edu.cn/api/lxsp/post_lxsp_sm_test20210311.php",
     method: "POST",
     data: {
@@ -36,4 +47,3 @@ export const apply = (type: Type, location: Location) => {
       "content-type": "application/json",
     },
   });
-};
